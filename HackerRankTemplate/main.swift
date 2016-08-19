26f84eca7cc30ffe39d1b60e0374c904c6821745
Andrew Ebling <andrew@ebling.me.uk>
2016-08-19 14:46:29 +0100

    Initial Commit

---
+++ HackerRankTemplate/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HackerRankTemplate
+//
+//  Created by Andrew Ebling on 19/08/2016.
+//  Copyright © 2016 Tenero. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
